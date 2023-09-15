f47f0deb5b6434dca38c3e80d5f0f2c78423192e
Ko1da3vN1k1ta <maniac779@mail.ru>
2023-09-15 11:24:19 +0300

    Initial commit

---
+++ src/main/java/org/example/CheckingExceptions.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.Scanner;
+public class CheckingExceptions {
+    public int nextInt() {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        try {
+            x = sc.nextInt();
+            return x;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
